3e5cce4515516f00e468ec1b66ea53ab422d1978
mjstromberg <michaeljstromberg@gmail.com>
2016-10-05 16:48:51 -0700

    Make basic request handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var request = require('request');
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // make request function
+  // console.log('method', req);
+
+  // if req.method === GET
+  if (req.method === 'GET') {
+    // if req.url = '/'
+    if (req.url === '/') {
+      // grab index.html file
+      fs.readFile(path.join(__dirname, '../web/public/index.html'), function(error, content) {
+        if (error) {
+          console.log(error);
+        } else {
+          // serve up our html
+          console.log('working');
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+        }
+      });
+    }
+  }
+  // console.log('resp', res);
+  // res.end(path.join(__dirname, '../web/public/index.html'));
+  // res.end(archive.paths.list);
 };
