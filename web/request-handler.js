1e397bb2decbe9b06e0a3495f1844eb5357617b5
mjstromberg <michaeljstromberg@gmail.com>
2016-10-05 17:28:56 -0700

    Start making URL handling for archived pages

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,25 +6,56 @@ var fs = require('fs');
 exports.handleRequest = function (req, res) {
   // make request function
   // console.log('method', req);
-
+  var filePath;
+  var contentType;
   // if req.method === GET
   if (req.method === 'GET') {
     // if req.url = '/'
     if (req.url === '/') {
       // grab index.html file
-      fs.readFile(path.join(__dirname, '../web/public/index.html'), function(error, content) {
+      filePath = path.join(__dirname, '../web/public/index.html');
+      var contentType = 'text/html';
+    } else {
+      // get whatever is in sites.txt
+      var sites;
+
+      fs.readFile(path.join(__dirname, '../archives/sites.txt'), function(error, content) {
         if (error) {
           console.log(error);
         } else {
-          // serve up our html
-          console.log('working');
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf8');
+          sites = content;
         }
       });
+      // if url is in sites.txt
+      if (sites[req.url]) {
+        // set filePath to sites/url
+        filePath = path.join(__dirname, '../archives/sites' + req.url);
+        // set contentType to 'text/html'
+        contentType = 'text/html';
+      // else
+      } else {
+        // set filePath to loading.html
+        filePath = path.join(__dirname, '../web/public/loading.html');
+        // set contentType to 'text/html'
+        var contentType = 'text/html';
+        // add url to sites.txt
+
+        // save html file in sites folder
+        
+      }
     }
   }
-  // console.log('resp', res);
-  // res.end(path.join(__dirname, '../web/public/index.html'));
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      console.log(error);
+    } else {
+      // serve up our html
+      console.log('working');
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+
   // res.end(archive.paths.list);
 };
